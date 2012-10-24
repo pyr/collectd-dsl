dc5095727877a7e43db3f09e2f88a9fa9ab0bd4d
Bryan Berry <bryan.berry@gmail.com>
2012-10-24 18:54:00 +0200

    closes #1 convert snake-case to CamelCase
    let' use snake-case for the DSL as it is the Ruby
    convention, but convert the output to CamelCase
    per collectd configuration format. also puts quotes
    around strings but not booleans.

--- lib/collectd-dsl.rb
+++ lib/collectd-dsl.rb
@@ -20,18 +20,30 @@ module Collectd
       ("\t" * @indent) + str
     end
 
+    def snake_to_camel method_name
+      method_name.split("_").map{|w| w.capitalize }.join("")
+    end
+
     def method_missing method_name, *args
-      mapped_args = args.map{|a| a.to_s}.join(" ")
+      mapped_args = args.map do |a|
+        if a.class == String 
+          "\"" + a.to_s + "\""
+        else
+          a.to_s
+        end
+      end.join(" ")
+        
       mapped_args = (" " + mapped_args) unless mapped_args.empty?
 
+      camel_method_name = snake_to_camel method_name.to_s
       if block_given?
-        @directives << indent("<#{method_name.to_s}#{mapped_args}>")
+         @directives << indent("<#{camel_method_name}#{mapped_args}>")
         @indent += 1
         yield # rely on the outer scope
         @indent -= 1
-        @directives << indent("</#{method_name.to_s}>")
+        @directives << indent("</#{camel_method_name}>")
       else
-        @directives << indent("#{method_name.to_s}#{mapped_args}")
+        @directives << indent("#{camel_method_name}#{mapped_args}")
       end
     end
   end
